e4118ddb6aea58608aa028dc8b85e6d491fb2319
developer <heriqnazsim@gmail.com>
2020-07-06 23:41:15 +0400

    new

---
+++ build/precache-manifest.80f7b28d5155aa6158ddeaca689e3042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f337ea14c10618cd822",
+    "url": "/static/css/main.76c562c5.chunk.css"
+  },
+  {
+    "revision": "8f337ea14c10618cd822",
+    "url": "/static/js/main.8f337ea1.chunk.js"
+  },
+  {
+    "revision": "3372a3bfa52ee185a71e",
+    "url": "/static/js/1.3372a3bf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "369029359613eab5881bcab31043bc54",
+    "url": "/index.html"
+  }
+];
